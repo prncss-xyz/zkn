5da937dc2c1f237e77c914370bf0ff7deb17bbe8
Juliette Lamarche <princesse@princesse.xyz>
2023-07-02 14:45:51 -0400

    fix: CI tests

---
+++ scripts/run-e2e.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+BASE=$(mktemp /tmp/zkn.XXXXXX)
+DATABASE_URL=file:"$BASE"
+export DATABASE_URL
+pnpm prisma migrate dev --name init
+if [ "$#" -eq "0" ]; then
+	vitest -c ./vitest.config.integration.ts
+	pnpm exec playwright test
+else
+	pnpm exec playwright test --ui
+fi
+rm "$BASE"

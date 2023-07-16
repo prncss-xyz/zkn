f9c0951f85a7fc29b666a03f1936bbf69a8924a3
Juliette Lamarche <princesse@princesse.xyz>
2023-07-16 10:25:03 -0400

    fix: works without config file

--- src/server/data/notebookConfig.ts
+++ src/server/data/notebookConfig.ts
@@ -4,20 +4,30 @@ import { readFile } from "node:fs/promises";
 import { load } from "js-yaml";
 import { z } from "zod";
 
-const conf = z.object({
-  kanbans: z.record(z.string(), z.array(z.string())),
-  reversed_tags: z.optional(z.array(z.string())),
-});
+const kanbansSchema = z.record(z.string(), z.array(z.string())).default({});
+const reversedTagsSchema = z.array(z.string()).default([]);
 
-async function getNotebookConfig_() {
+async function loadConfig(): Promise<any> {
   const file = path.join(notebookDir, ".notebook.yaml");
   let raw: string | null = null;
   try {
     raw = await readFile(file, "utf8");
-  } catch (err) {}
-  const obj = raw ? load(raw) : {};
-  const res = conf.parse(obj);
-  return { kanbans: res.kanbans ?? {}, reversedTags: res.reversed_tags ?? [] };
+  } catch (err) {
+    return {};
+  }
+  const obj = load(raw);
+  if (typeof obj !== "object")
+    throw new Error("Config file should be a yaml object");
+  if (obj === null) throw new Error("Config file should be a yaml object");
+  return obj;
+}
+
+async function getNotebookConfig_() {
+  const obj = await loadConfig();
+  return {
+    kanbans: kanbansSchema.parse(obj.kanbans),
+    reversedTags: reversedTagsSchema.parse(obj.reversed_tags),
+  };
 }
 let config: ReturnType<typeof getNotebookConfig_>;
 
